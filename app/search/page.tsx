4c97865d1099f4a7dd38ae069dbda48388a0cc78
ganeshb2334 <bastapureganesh21@gmail.com>
2024-08-29 16:25:37 +0530

    Updated

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -3,20 +3,42 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { SearchIcon } from "lucide-react";
 import { motion } from "framer-motion";
+import { moveDown, moveLeft, moveRight, moveUp } from "../utils/motion";
 
 const Search = () => {
   return (
-    <div className="text-white w-screen h-[calc(100vh-5rem)]">
+    <div className="text-white w-screen h-[calc(100vh-5rem)] overflow-hidden">
       <div className="w-full border p-3">
         <div className="flex items-center gap-3">
-          <motion.div className="flex-shrink-0">
+          <motion.div
+            variants={moveRight(0, 0.3)}
+            initial="hidden"
+            animate="visible"
+            className="flex-shrink-0"
+          >
             <SearchIcon className="" />
           </motion.div>
-          <motion.div className="rounded-full flex-grow">
-            <Input className="rounded-full" type="text" placeholder="Search here..." />
+          <motion.div
+            variants={moveUp(0, 0.3)}
+            initial="hidden"
+            animate="visible"
+            className="rounded-full flex-grow"
+          >
+            <Input
+              className="rounded-full"
+              type="text"
+              placeholder="Search here..."
+            />
           </motion.div>
-          <motion.div className="rounded-full ">
-            <Button className="rounded-full" variant="outline">Search</Button>
+          <motion.div
+            variants={moveLeft(0, 0.3)}
+            initial="hidden"
+            animate="visible"
+            className="rounded-full "
+          >
+            <Button className="rounded-full" variant="outline">
+              Search
+            </Button>
           </motion.div>
         </div>
       </div>
